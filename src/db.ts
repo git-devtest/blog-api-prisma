cd9ff9192df10a57e632077f594d58bfb5611330
Jhon Harold <jhonmaster+git@gmail.com>
2026-01-19 20:06:01 -0500

    chore: creación del adaptador prisma para sqlite

---
+++ src/db.ts
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "@prisma/client";
+
+const db = process.env.DATABASE_URL || "./dev.db";
+const adapter = new PrismaBetterSqlite3({ url: db });
+export const prisma = new PrismaClient({ adapter });
